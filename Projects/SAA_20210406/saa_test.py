958cd40fded684b0a2084d69cbea1abd85449416
mmi <mmi@ngssuper.com.au>
2021-06-08 02:13:17 +1000

    modify port_opt.py and saa_test.py to modify saa_constraints_check() function

--- Projects/SAA_20210406/saa_test.py
+++ Projects/SAA_20210406/saa_test.py
@@ -39,22 +39,18 @@ args_obj = {'max_sharpe': (exp_ret, exp_cov, 0.0025, True),
 saa_opt = SAAOpt(map_saa, cons_saa, options, n_assets=N,
                  tickers=sub_sectors, bounds=bounds)
 res_multi_options = saa_opt.options_opt_run(obj_list, args_obj, bounds=bounds,
-                                            print_res=False)
+                                            print_res=False,
+                                            check_constraints=True)
 
-wgt_multi_options = saa_opt.export_saa_output(save_excel=False,
+wgt_multi_options = saa_opt.export_saa_output(save_excel=True,
                                               filename='saa_output.xlsx')
 
-df_saa_wgt = saa_opt.df_wgt
-
-for (a, b) in df_saa_wgt.columns:
-    print(a)
-    print(df_saa_wgt.loc[:, (a, b)])
 
 
 
 
 
-
+"""
 # Test one option and one objective function
 saa_opt = SAAOpt(map_saa, cons_saa, option_i, n_assets=N,
                  tickers=sub_sectors, bounds=bounds)
@@ -74,67 +70,4 @@ saa_opt.map_saa_constraints(option_i)
 res_multi_obj = saa_opt.opt_run(obj_list, args_obj, bounds=bounds,
                                 print_res=True)
 wgt_multi_obj = saa_opt.export_wgt()
-
-
-"""-------Storage for map_saa_constraint()-----
-for i in range(len(cons.index)):
-    con_i = cons.index[i]
-    con_type_i = cons['Con_Type'].iloc[i]
-    type_i = cons['Type'].iloc[i]
-    if (con_type_i == 'Sum') & (type_i == 'Sum'):
-        sum_i = cons.loc[con_i, option_i]
-        saa_opt.add_constraint(
-            'eq', 'lambda x: x.sum() - {}'.format(sum_i))
-    elif con_i in map_saa.columns[2:]:
-        if type_i == 'Min':
-            x_min_i = cons[option_i].iloc[i]
-            saa_opt.add_constraint(
-                'ineq', "lambda x: x.T @ map_saa.loc[:, con_type_i] - {}".format(
-                    x_min_i))
-        elif type_i == 'Max':
-            x_max_i = cons[option_i].iloc[i]
-            saa_opt.add_constraint(
-                'ineq', "lambda x: {} - x.T @ map_saa.loc[:, con_type_i]".format(
-                    x_max_i))
-        else:
-            raise Warning(
-                'Constraint {} is not added for wrong format.'.format(con_i))
-    elif con_type_i == 'Sector_PDS':
-        # Get the location of subsectors that belong to the sector.
-        # E.g., for Australian Equities,
-        # [0, 1] in the map_saa.Sector_PDS vector.
-        loc_sector_pds_i = np.where(map_saa.Sector_PDS == con_i)
-
-        # Check min or max
-        if type_i == 'Min':
-            x_min_i = cons[option_i].iloc[i]
-            saa_opt.add_constraint(
-                'ineq', "lambda x: x[{}].sum() - {}".format(
-                    loc_sector_pds_i, x_min_i))
-        elif type_i == 'Max':
-            x_max_i = cons[option_i].iloc[i]
-            saa_opt.add_constraint(
-                'ineq', "lambda x: {} - x[{}].sum()".format(
-                    x_max_i, loc_sector_pds_i))
-        else:
-            raise Warning(
-                'Constraint {} is not added for wrong format.'.format(con_i))
-                
-# Test - add constraint one by one
-saa_opt.add_constraint('eq', "lambda x: x.sum() - 1")
-
-con_i = 'Australian Equities'
-x_min_i = 0.15
-x_max_i = 0.4
-
-loc_sector_pds_i = np.where(map_saa.Sector_PDS == con_i)[0]
-saa_opt.add_constraint('ineq', "lambda x: x[{}].sum() - {}".format(
-        list(loc_sector_pds_i), x_min_i))
-saa_opt.add_constraint('ineq', "lambda x: {} - x[{}].sum()".format(
-        x_max_i, list(loc_sector_pds_i)))
-
-saa_opt.add_constraint('ineq', "lambda x: x.T @ np.array({}) - {}".format(
-                            list(map_saa.loc[:, con_i]), x_min_i))
-saa_opt.add_constraint('ineq', "lambda x: {} - x.T @ np.array({})".format(
-                            x_max_i, list(map_saa.loc[:, con_i])))
 """
